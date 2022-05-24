2c64f1c4d7c84815ab1c271c26050a3e63aa0e45
FilipRedlich <filipredlich.pr@gmail.com>
2022-05-24 18:14:15 +0200

    added few test func

---
+++ main.py
@@ -0,0 +1,25 @@
+import pyautogui
+
+def findMousePosition():
+    try:
+        while True:
+            x, y = pyautogui.position()
+            positionStr = 'X: ' + str(x).rjust(4) + ' Y: ' + str(y).rjust(4)
+            print(positionStr, end='')
+            print('\b' * len(positionStr), end='', flush=True)
+    except KeyboardInterrupt:
+        print("\nDone")
+
+def pressKey(key):
+    try:
+        while True:
+            pyautogui.press(key,presses=1,interval=0.5)
+    except KeyboardInterrupt:
+        print("\nDone")
+
+if __name__ == '__main__':
+    width, height = pyautogui.size()
+    findMousePosition()
+    pressKey("F")
+
+    
