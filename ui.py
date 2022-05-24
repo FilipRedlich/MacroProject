4ae61399df6e8272e38d82bcd15a2a3b596cbbc8
FilipRedlich <filipredlich.pr@gmail.com>
2022-05-24 18:37:16 +0200

    changed script creation to allow new lines

--- ui.py
+++ ui.py
@@ -1,8 +1,8 @@
 def createScript():
     f = open("customScript.py", "w")
     f.write("""import macro
-if __name__ == '__main__':
-    macro.pressKey('E',3)\n""")
+if __name__ == '__main__':\n""")
+    f.write("\tmacro.pressKey('E',3)")
     f.close()
 
 if __name__ == '__main__':
