5afc2e107032489aebedd1fe11d4215d6e06cceb
FilipRedlich <filipredlich.pr@gmail.com>
2022-05-24 18:49:23 +0200

    made basic UI

--- ui.py
+++ ui.py
@@ -1,3 +1,8 @@
+import tkinter as tk
+
+def loadScript():
+    0
+
 def createScript():
     f = open("customScript.py", "w")
     f.write("""import macro
@@ -6,4 +11,18 @@ if __name__ == '__main__':\n""")
     f.close()
 
 if __name__ == '__main__':
-    createScript()
+    root = tk.Tk()
+
+    canvas = tk.Canvas(root, height=600, width=600, bg="lightblue")
+    canvas.pack()
+
+    frame = tk.Frame(root, bg="white")
+    frame.place(relwidth=0.8,relheight=0.7,relx=0.1,rely=0.1)
+
+    openFile = tk.Button(root, text="Load Script", fg="black", bg="white", padx=7, pady=3, command=loadScript)
+    openFile.pack(fill="both")
+
+    saveFile = tk.Button(root, text="Create Script", fg="black", bg="white", padx=7, pady=3, command=createScript)
+    saveFile.pack(fill="both")
+
+    root.mainloop()
